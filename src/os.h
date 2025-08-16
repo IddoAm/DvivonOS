974914863dc042d0222ed480e32d4bd774b8da65
Iddo <amraniiddo@gmail.com>
2025-08-16 22:52:48 +0300

    os file

---
+++ src/os.h
@@ -0,0 +1,17 @@
+#ifndef OS_H
+#define OS_H
+
+#include <stdint.h>
+
+static inline uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    __asm__ __volatile__("inb %1, %0" : "=a"(ret) : "dN"(port));
+    return ret;
+}
+
+
+static inline void outb(uint16_t port, uint8_t data) {
+    __asm__ __volatile__("outb %0, %1" : : "a"(data), "dN"(port));
+}
+
+#endif 
